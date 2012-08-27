bc04259a7229cfe888ee1ec7892496e8bb724012
Victor Widell <victor@topmost.se>
2012-08-27 16:01:42 +0200

    Added some more style settings.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -38,28 +38,41 @@ var EditSiteView = Backbone.View.extend({
 			},
 			style: {
 				h2: {
-					"font-family": "Comic Sans MS",
+					"font-family": "Georgia",
 					color: "#5cf",
-					"font-size": "24px",
-					"font-weight": "normal"
+					"font-size": "36px",
+					"font-weight": "normal",
+					"font-style": "normal"
+				},
+				a: {
+					color: "#f4a",
+					"font-weight": "normal",
+					"font-style": "normal",
+					"text-decoration": "none"
 				},
 				p: {
+					color: "#ccc",
+					"font-size": "16px",
 					"font-family": "Verdana",
-					color: "black",
-					"font-size": "16px"
+					"font-weight": "normal",
+					"font-style": "normal"
 				},
 				blockquote: {
+					color: "#5cf",
+					"font-size": "20px",
 					"font-family": "Verdana",
-					color: "#f4a",
-					"font-size": "18px"
+					"font-weight": "normal",
+					"font-style": "italic"
 				},
 				lead: {
-					color: "black",
-					"font-weight": "bold",
-					"font-size": "20px"
+					color: "#eee",
+					"font-size": "20px",
+					"font-family": "Verdana",
+					"font-weight": "normal",
+					"font-style": "normal"
 				},
 				background: {
-					"background-color": "#eee"
+					"background-color": "#444"
 				}
 			}
 		});
